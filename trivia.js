758d38fac757042b1a267b0336f270b61d14e155
Allison O'Hanlon <allison.t.ohanlon@gmail.com>
2017-05-12 22:05:01 -0500

    doneeeeee

--- trivia.js
+++ trivia.js
@@ -1,6 +1,49 @@
-var questionClasses = [$("#questionOne"), $("#questionTwo"), $("#questionThree"), $("#questionFour") ]
+var questionId = [$("#questionOne"), $("#questionTwo"), $("#questionThree"), $("#questionFour") ];
+var points = 0;
 
 //changing the box when the "next button is clicked"
+
+//making user's answer 
+var clickOne = 0;
+var clickTwo = 0;
+var clickThree = 0;
+var clickFour = 0;
+
+    $(".answerOne").on("click", function() {
+	clickOne++;
+		
+	if (clickOne === 1) {
+	$(this).attr("class", "UserAnswerOne");
+	$(this).css({"background-color": "black", "color": "white"});
+	}
+	});
+
+	$(".answerTwo").on("click", function() {
+	clickTwo++;
+		
+	if (clickTwo === 1) {
+	$(this).attr("class", "UserAnswerTwo");
+	$(this).css({"background-color": "black", "color": "white"});
+	}	
+	});
+	
+	$(".answerThree").on("click", function() {
+	clickThree++;
+		
+	if (clickThree === 1) {
+	$(this).attr("class", "UserAnswerThree");
+	$(this).css({"background-color": "black", "color": "white"});
+	}
+	});
+
+	$(".answerFour").on("click", function() {
+	clickFour++;
+		
+	if (clickFour === 1) {
+	$(this).attr("class", "UserAnswerFour");
+	$(this).css({"background-color": "black", "color": "white"});
+	}
+	});
 
 //use boolen to stop things
 	$(".start").click(function() {
@@ -33,40 +76,72 @@ var questionClasses = [$("#questionOne"), $("#questionTwo"), $("#questionThree")
 	$(".submit").click(function() {
 		$("#questionFour").toggle();
 
-		for (i = 0; i < questionClasses.length; i++) {
-		questionClasses[i].toggle();
-		}
+		for (i = 0; i < questionId.length; i++) {
+		questionId[i].toggle();
+		};
 
-		$("button").remove();
+		$(".UserAnswerOne").css({"background-color": "white", "color": "black"});
+		$(".UserAnswerTwo").css({"background-color": "white", "color": "black"});
+		$(".UserAnswerThree").css({"background-color": "white", "color": "black"});
+		$(".UserAnswerFour").css({"background-color": "white", "color": "black"});
+
+		$("button").empty();
 		$("p").css({"border": "none", "padding": "0"});
 
 		$("#time-left").css("visibility", "hidden");
 		countdown.reset();
 		countdown.stop();
-	});
-
-//making user's answer 
-	$(".answerOne").click(function() {
-	$(this).attr("class", "UserAnswerOne");
-	});
-
-	$(".answerTwo").click(function() {
-	$(this).attr("class", "UserAnswerTwo");
-	});
-
-	$(".answerThree").click(function() {
-	$(this).attr("class", "UserAnswerThree");
-	});
-
-	$(".answerFour").click(function() {
-	$(this).attr("class", "UserAnswerFour");
-	});
 
 	var correctAnswerOne = document.getElementById('correctAnswerOne').className.split(/\s+/);
+	var correctAnswerTwo = document.getElementById('correctAnswerTwo').className.split(/\s+/);
+	var correctAnswerThree = document.getElementById('correctAnswerThree').className.split(/\s+/);	
+	var correctAnswerFour = document.getElementById('correctAnswerFour').className.split(/\s+/);
 
 	if (correctAnswerOne.indexOf('UserAnswerOne') > -1) {
-		$("#correctAnswerOne").css("color", "green");
+		$("#correctAnswerOne").css({"color": "green", "font-weight": "bold"});
+		points++;
 	}
+	else {
+		$("#correctAnswerOne").css({"color": "green", "font-weight": "bold"});
+		$(".UserAnswerOne").css({"color": "red", "font-weight": "bold"});
+	}	
+
+	if (correctAnswerTwo.indexOf('UserAnswerTwo') > -1) {
+		$("#correctAnswerTwo").css({"color": "green", "font-weight": "bold"});
+		points++;
+	}
+	else {
+		$("#correctAnswerTwo").css({"color": "green", "font-weight": "bold"});
+		$(".UserAnswerTwo").css({"color": "red", "font-weight": "bold"});
+	}
+
+	if (correctAnswerThree.indexOf('UserAnswerThree') > -1) {
+		$("#correctAnswerThree").css({"color": "green", "font-weight": "bold"});
+		points++;
+	}
+	else {
+		$("#correctAnswerThree").css({"color": "green", "font-weight": "bold"});
+		$(".UserAnswerThree").css({"color": "red", "font-weight": "bold"});
+	}
+
+	if (correctAnswerFour.indexOf('UserAnswerFour') > -1) {
+		$("#correctAnswerFour").css({"color": "green", "font-weight": "bold"});
+		points++;
+	}
+	else {
+		$("#correctAnswerFour").css({"color": "green", "font-weight": "bold"});
+		$(".UserAnswerFour").css({"color": "red", "font-weight": "bold"});
+	}
+
+	$("#resultBox").append("<h3>You got " + points + " out of 4 correct.</h3>");
+		if (points === 4) {
+			$("#resultBox").append("<h3><em><You are Beyonce.</em></h3>");
+		}
+
+	});
+
+
+
 
 
 //timer stuff
